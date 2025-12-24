080e8f2e972cdd98af81a034725e38ddafcc2660
Gaurav Kumar <gaurav31120@gmail.com>
2025-12-24 23:34:25 +0530

    test valid json

--- auth-service/src/routes/AuthController.ts
+++ auth-service/src/routes/AuthController.ts
@@ -1,9 +1,8 @@
-import {type Request, type Response} from 'express'
-
-
+import { type Request, type Response } from 'express'
 
 export class AuthController {
-register(req:Request, res:Response) {
-    res.status(201).send();
-}
+    register(req: Request, res: Response) {
+        // res.status(201).send()
+        res.status(201).json()
+    }
 }
